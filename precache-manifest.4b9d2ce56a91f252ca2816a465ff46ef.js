fd8ec89e1a079cfabcaecac58ad027565b52b13e
Andy Hansen <ahansen.nz@gmail.com>
2019-09-14 13:41:54 +1200

    Updates

---
+++ precache-manifest.4b9d2ce56a91f252ca2816a465ff46ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f9e553d55b7bf9b386ca88fe8c496a",
+    "url": "/feral-hog-distribution/index.html"
+  },
+  {
+    "revision": "235a3b9ea24a28b7847a",
+    "url": "/feral-hog-distribution/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a989474a6dc345659ab2",
+    "url": "/feral-hog-distribution/static/js/2.77f317fe.chunk.js"
+  },
+  {
+    "revision": "235a3b9ea24a28b7847a",
+    "url": "/feral-hog-distribution/static/js/main.fde021af.chunk.js"
+  },
+  {
+    "revision": "6492a628f33087008348",
+    "url": "/feral-hog-distribution/static/js/runtime~main.fe5fbbe4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/feral-hog-distribution/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ed1462fbd75e476caef7bfe631215f3.js"
+  "/feral-hog-distribution/precache-manifest.4b9d2ce56a91f252ca2816a465ff46ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/feral-hog-distribution/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
