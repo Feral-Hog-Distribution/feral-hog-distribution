4631e15f3575184d26d10983680bda3ded0411a5
Andy Hansen <ahansen.nz@gmail.com>
2019-09-15 13:36:29 +1200

    make a noise when button is clicked

--- src/Boop/Boop.js
+++ src/Boop/Boop.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+const audio = new Audio('./hog11.mp3')
 export default class Boop extends React.Component {
   render() {
     return (
-      <p><button onClick={() => this.props.onBoop(1)} type="button" className="text" name="input">Snoot</button></p>
+      <p><button onClick={() => {this.props.onBoop(1); audio.play()}} type="button" className="text" name="input">Snoot</button></p>
     )
   }
 }
