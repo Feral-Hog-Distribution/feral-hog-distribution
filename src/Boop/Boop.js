87c87324b875130fe10af0290629614ee2912847
sophimayp <sophimayp@gmail.com>
2019-09-15 14:18:04 +1200

    added images for pig and ship, added chonk scss and js

--- src/Boop/Boop.js
+++ src/Boop/Boop.js
@@ -3,7 +3,27 @@ import React from 'react'
 export default class Boop extends React.Component {
   render() {
     return (
-      <p><button onClick={() => this.props.onBoop(1)} type="button" className="text" name="input">Snoot</button></p>
+      <form id="module_boop" className="module" action="#booped">
+        <button onClick={() => this.boopIt()} type="button" className="text" name="input">Snoot</button>
+        <div id="chonk">Chonk</div>
+      </form>
     )
+  }
+
+  boopIt = () => {
+    this.props.onBoop(1);
+    console.log("its a boopy doop");
+    var element = document.getElementById("chonk");
+    if(element){
+      element.classList.add("booped");
+      setTimeout(this.chonkSad, 700)
+    }
+  }
+
+  chonkSad = () => {
+    var element = document.getElementById("chonk");
+    if(element){
+      element.classList.remove("booped");
+    }
   }
 }
